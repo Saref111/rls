850a9bd2f7505380ed93d547e95124fe427630a0
Ivan Borovyk <saref012@gmail.com>
2025-01-13 16:56:11 +0200

    refactored

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,19 @@ impl Default for Config {
 fn main() {
     let args: Vec<String> = args().skip(1).collect();
 
-    let config = build_config(args);
+    let config = match build_config(args) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("{e}");
+            exit(1);
+        }
+    };
+
+    if !config.path.is_dir() {
+        eprintln!("Provided path is not a directory");
+        exit(1);
+    }
+
     let entries = match config.path.read_dir() {
         Ok(entries) => entries,
         Err(e) => {
@@ -23,18 +35,39 @@ fn main() {
         }
     };
 
-    entries.for_each(|e| println!("{}", e.unwrap().path().display()));
+    for entry in entries {
+        match entry {
+            Ok(e) => {
+                let file_name = e.file_name();
+                let name = file_name.to_string_lossy();
+
+                if config.show_all || !name.starts_with(".") {
+                    println!("{}", e.path().display());
+                }
+            },
+            Err(e) => {
+                eprintln!("Cannot read entry: {}", e);
+            }
+        }
+    }
 }
 
 
-fn build_config(args: Vec<String>) -> Config {
-    args.into_iter().fold( Config::default(), |mut acc, it| {
-        match it.as_str() {
-            "-s" | "--show-all" => acc.show_all = true,
-            string if !string.starts_with("-") => acc.path = PathBuf::from_str(string).expect("Provided incorrect path"), 
-            _ => {}
-        };
+fn build_config(args: Vec<String>) -> Result<Config, String> {
+    let mut config = Config::default();
 
-        acc
-    })
+    for arg in args {
+        match arg.as_str() {
+            "-a" | "--show-all" => config.show_all = true,
+            string if !string.starts_with('-') => {
+                config.path = PathBuf::from(string);
+                if !config.path.exists() {
+                    return Err(format!("Provided path does not exist: {}", config.path.display()));
+                }
+            }
+            _ => return Err(format!("Unknown argument: {}", arg)),
+        }
+    }
+
+    Ok(config)
 }
